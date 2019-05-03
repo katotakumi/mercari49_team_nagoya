102d4847362e44b7e0773de6f2111acbc651f4e8
silk-k <aomine1010@gmail.com>
2019-05-03 12:08:37 +0900

    コントローラー作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,26 @@
 class TopsController < ApplicationController
   def index
   end
+
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+     
+  end
+  
+def destroy
+  
+end
+
+def edit
+  
+end
+
+def update
+end
+  
 end
