ec0af1d340ff6e2154a0b552fc06af9a20f9778d
katotakumi <katotaku0084@gmail.com>
2019-05-07 12:41:19 +0900

    コントローラー　不要なコード　削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,6 @@ class UsersController < ApplicationController
     
   end
   def show
-    # 現在ログインしているユーザーであるか
-    user = User.find(params[:id])
     @nickname = current_user.nickname
     # exhibitionsテーブルのproduct_name(カラム名は仮置き)の最新4件を取得 
     @exhibitions = exhibitions.product_name.page(params[:page]).per(4).order("created_at DESC")
