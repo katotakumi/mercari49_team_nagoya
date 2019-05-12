3ddf0fd98c37eb584a2e0ef6f351fc1150950127
silk-k <aomine1010@gmail.com>
2019-05-12 15:57:10 +0900

    Merge branch 'master' into 商品詳細ページ（サーバー）
    # Conflicts:
    #	app/assets/stylesheets/application.scss
    #	db/schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     # omniauth_callbacks: 'users/omniauth_callbacks',
   }
   root 'tops#index'
-  resources :users, only: [:index, :edit, :update, :show]
+  resources :users, only: [:index, :edit, :update, :show] do
+    collection do
+      get :logout
+    end
+  end
   resources :tops, only: [:index, :new, :create, :show, :edit, :update] 
 
   resources :tops do
@@ -16,5 +20,6 @@ Rails.application.routes.draw do
       patch 'buy'
     end
   end
+
 
 end
